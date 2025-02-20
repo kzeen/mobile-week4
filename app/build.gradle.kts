7d11557997e70b15d492cf68a392ccf07ae678dd
zeeny <157370421+kzeen@users.noreply.github.com>
2025-02-20 15:54:24 +0200

    Activate view binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+//    Activate view binding
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
